55305597646a513ed5e26344fa1ee4848dc88a68
Milos Milinkov <milos300399@gmail.com>
2024-03-06 10:53:23 +0100

    v0.4.1 Enabled linear mod

--- client/src/app/shared/components/stepper/stepper.component.ts
+++ client/src/app/shared/components/stepper/stepper.component.ts
@@ -7,15 +7,14 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./stepper.component.scss'],
   providers: [{provide: CdkStepper, useExisting: StepperComponent}]
 })
-export class StepperComponent extends CdkStepper implements OnInit{
- 
+export class StepperComponent extends CdkStepper implements OnInit {
   @Input() linearModeSelected = true;
 
   ngOnInit(): void {
     this.linear = this.linearModeSelected;
   }
 
-  onClick(index: number){
+  onClick(index: number) {
     this.selectedIndex = index;
   }
 }
